0cee1be1afd808da4a3959d90039950cdeb7dcff
Adam Rynczak <karolat@gmail.com>
2015-05-19 02:41:45 -0400

    notes

--- src/LTournament/client/LTournamentService.java
+++ src/LTournament/client/LTournamentService.java
@@ -5,7 +5,9 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("LTournamentService")
-public interface LTournamentService extends RemoteService {
+public interface
+
+        LTournamentService extends RemoteService {
     // Sample interface method of remote interface
     String getMessage(String msg);
 
